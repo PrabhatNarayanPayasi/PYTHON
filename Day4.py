44daf75d32616821a7a6c5dca700a462a034f315
PrabhatNarayanPayasi <142197070+PrabhatNarayanPayasi@users.noreply.github.com>
2024-11-19 19:26:37 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,16 @@
+print("hello Prabhat!!")
+# Print ek function h jise invoke karne ke liye () use hotah hai
+print(6)
+print(5)
+print("PRABHAT",66 , "\n ppp")
+print(3+7 , 3*3 , 7/6 , 5-2)
+
+
+# Day 4 - Our First Program
+# Today we will write our first ever python program from scratch. It will consist of a bunch of print statements.
+# print can be used to print something on the console in python.
+
+ 
+
+print("---Your poem here---")
+ 
